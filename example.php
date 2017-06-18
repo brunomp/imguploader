dd66496219dedaf6a0ac521b97183966b6e13a75
Bruno Pereira <brunomarcospereira@gmail.com>
2017-06-18 18:21:05 -0300

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   //header('HTTP/1.1 500 Internal Server Error');
   header('HTTP/1.1 200 OK');
   $imageBlob = base64_decode(explode(',', $_POST['imagem'], 2)[1]);
-  file_put_contents('teste.jpg', $imageBlob);
+  file_put_contents(time().'.jpg', $imageBlob);
   exit;
 }
 
@@ -22,17 +22,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div id="upload">
   <input style="display: none;" id="inputImagem" type="file" multiple="true" accept="image/*" />
   <button onclick="document.getElementById('inputImagem').click();">Selecionar Fotos</button>
-  <div id="imagensFila"></div>
+  <div id="imagensFila" class="imguploader"></div>
 </div>
 
-<script src="/imguploader.min.js" type="text/javascript"></script>
+<script src="/imguploader.js" type="text/javascript"></script>
 <script type="text/javascript">
 
 var uploader = new ImgUploader({
   inputFileId: 'inputImagem',
   reduceWidth: 1000,
   reducetHeight: 1000,
-  target: '/',
+  target: window.location.href,
   containerId: 'imagensFila',
   paramName: 'imagem',
   btnCancelText: 'Cancelar',
